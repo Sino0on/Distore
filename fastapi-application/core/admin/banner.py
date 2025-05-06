642cb05a98007ddc5c820f37734a263355d15556
Dastan <dastan.kubanuulu1@gmail.com>
2025-05-06 20:18:20 +0600

    fix nginx conf

--- fastapi-application/core/admin/banner.py
+++ fastapi-application/core/admin/banner.py
@@ -3,30 +3,33 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqladmin import ModelView
 from wtforms import TextAreaField
 from core.models import Banner, Product
+from sqlalchemy.orm import object_session
 from sqlalchemy.future import select
 
 
 class BannerAdmin(ModelView, model=Banner):
     column_list = ["id", "title", "created_at"]
-    form_columns = ["title", "title_ky", "title_en", "description", "description_ky", "description_en", "image", "product_ids", 'status']  # Убедись, что это поле в списке
+    form_columns = ["title", "title_ky", "title_en", "description", "description_ky", "description_en", "image", "product_ids", 'status']
 
     form_extra_fields = {
         "product_ids": TextAreaField("Product IDs (comma-separated)")
     }
 
-    async def on_model_change(self, data, model, is_created, request: Request):
-        # Получаем сессию через зависимость
-        session: AsyncSession = request.state.session
-
-        # Получаем ids продуктов, разделенные запятой
+    async def on_model_change(self, data, model: Banner, is_created: bool, request: Request):
+        # Получаем product_ids из формы
         product_ids_raw = data.pop("product_ids", "")
         id_list = [int(pid.strip()) for pid in product_ids_raw.split(",") if pid.strip().isdigit()]
 
-        # Загружаем продукты по этим ID
-        result = await session.execute(
-            select(Product).where(Product.id.in_(id_list))
-        )
+        # Используем сессию, к которой уже прикреплён model
+        session: AsyncSession = object_session(model)
+
+        # Если object_session вернул None (вдруг), fallback на state.session
+        if session is None:
+            session = request.state.session
+
+        # Загружаем продукты
+        result = await session.execute(select(Product).where(Product.id.in_(id_list)))
         products = result.scalars().all()
 
-        # Связываем продукты с баннером
+        # Привязываем продукты к баннеру
         model.products = products
